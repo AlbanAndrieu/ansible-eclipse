3ba2939d6106de957c48f6df42b86c1004f959cd
Alban Andrieu <alban.andrieu@free.fr>
2020-11-19 10:26:27 +0100

    pre-commit

--- files/add_pydev_certificate.py
+++ files/add_pydev_certificate.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 # Add PyDev's certificate to Java's key and certificate database
 # Certificate file here: http://pydev.org/pydev_certificate.cer
-import os, sys, pexpect, urllib2
+import os
+import sys
+import pexpect
+import urllib2
+
 
 def find_java_home():
 
@@ -9,10 +14,10 @@ def find_java_home():
         return os.environ['JAVA_HOME']
 
     import subprocess
-    return_value=subprocess.call('which java',stdin=java_path)
+    return_value = subprocess.call('which java', stdin=java_path)
 
     if not return_value:
-        return java_path.replace('bin/java','')
+        return java_path.replace('bin/java', '')
 
     # fall back to original path..
     return '/usr/lib/jvm/default-java'
@@ -26,21 +31,23 @@ def main():
         raise ValuError('Wrong path to certs file')
 
     pydev_certs_url = 'http://pydev.org/pydev_certificate.cer'
-    print "Adding pydev_certificate.cer to %s" % (certs_file)
+    print 'Adding pydev_certificate.cer to %s' % (certs_file)
     pydev_cert = open('pydev_certificate.cer', 'w')
     pydev_cert.write(urllib2.urlopen(pydev_certs_url).read())
     pydev_cert.close()
-    cmd = "keytool -import -file ./pydev_certificate.cer -keystore %s" % (certs_file)
+    cmd = 'keytool -import -file ./pydev_certificate.cer -keystore %s' % (
+        certs_file)
     child = pexpect.spawn(cmd)
-    child.expect("Enter keystore password:")
-    child.sendline("changeit")
-    if child.expect(["Trust this certificate?", "already exists"]) == 0:
-        child.sendline("yes")
-    #try:
+    child.expect('Enter keystore password:')
+    child.sendline('changeit')
+    if child.expect(['Trust this certificate?', 'already exists']) == 0:
+        child.sendline('yes')
+    # try:
     #    child.interact()
-    #except OSError:
+    # except OSError:
     #    pass
-    print "done"
+    print 'done'
 
-if __name__ == "__main__":
-  main()
+
+if __name__ == '__main__':
+    main()
--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,9 +1,11 @@
+# -*- coding: utf-8 -*-
 import os
 
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ['MOLECULE_INVENTORY_FILE'],
+).get_hosts('all')
 
 
 def test_hosts_file(host):
